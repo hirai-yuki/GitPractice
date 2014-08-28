ff524884d5c8207cf0f5db516e181f0d5407c8d8
hirai.yuki <hirai.yuki@classmethod.jp>
2014-08-28 19:10:53 +0900

    TestClass2を作成しました。

---
+++ GitPractice/TestClass2.h
@@ -0,0 +1,13 @@
+//
+//  TestClass2.h
+//  GitPractice
+//
+//  Created by hirai.yuki on 2014/08/28.
+//  Copyright (c) 2014年 Classmethod, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestClass2 : NSObject
+
+@end
---
+++ GitPractice/TestClass2.m
@@ -0,0 +1,13 @@
+//
+//  TestClass2.m
+//  GitPractice
+//
+//  Created by hirai.yuki on 2014/08/28.
+//  Copyright (c) 2014年 Classmethod, Inc. All rights reserved.
+//
+
+#import "TestClass2.h"
+
+@implementation TestClass2
+
+@end
