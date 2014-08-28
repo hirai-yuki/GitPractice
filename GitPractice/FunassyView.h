6ee63622e530f9d638be8621e868200c8de319ef
cm-ttamiya <tamiya.tokihiko@classmethod.jp>
2014-08-28 19:12:47 +0900

    I added funassy

---
+++ GitPractice/FunassyView.h
@@ -0,0 +1,13 @@
+//
+//  FunassyView.h
+//  GitPractice
+//
+//  Created by tamiya.tokihiko on 2014/08/28.
+//  Copyright (c) 2014年 Classmethod, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface FunassyView : NSView
+
+@end
---
+++ GitPractice/FunassyView.m
@@ -0,0 +1,19 @@
+//
+//  FunassyView.m
+//  GitPractice
+//
+//  Created by tamiya.tokihiko on 2014/08/28.
+//  Copyright (c) 2014年 Classmethod, Inc. All rights reserved.
+//
+
+#import "FunassyView.h"
+
+@implementation FunassyView
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end
