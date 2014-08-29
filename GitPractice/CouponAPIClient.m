2ff1763f063bd87176da334d67f1a73d90abece7
cm-ttamiya <tamiya.tokihiko@classmethod.jp>
2014-08-29 15:58:27 +0900

    APIの作成完了

---
+++ GitPractice/CouponAPIClient.m
@@ -0,0 +1,13 @@
+//
+//  CouponAPIClient.m
+//  GitPractice
+//
+//  Created by tamiya.tokihiko on 2014/08/29.
+//  Copyright (c) 2014年 Classmethod, Inc. All rights reserved.
+//
+
+#import "CouponAPIClient.h"
+
+@implementation CouponAPIClient
+
+@end
