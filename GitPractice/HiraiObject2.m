65ccd1ea42af68afaada815c442f8ae5d2915e69
cm-ttamiya <tamiya.tokihiko@classmethod.jp>
2014-08-29 15:44:00 +0900

    I merged.

---
+++ GitPractice/HiraiObject2.m
@@ -0,0 +1,13 @@
+//
+//  HiraiObject2.m
+//  GitPractice
+//
+//  Created by hirai.yuki on 2014/08/29.
+//  Copyright (c) 2014年 Classmethod, Inc. All rights reserved.
+//
+
+#import "HiraiObject2.h"
+
+@implementation HiraiObject2
+
+@end
