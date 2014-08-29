9098ca324486a0ef44d91758917f7795a11dbc4d
notoroid <noto@irimasu.com>
2014-08-29 15:57:54 +0900

    CouponStatusManager を追加

---
+++ CouponStatusManager.h
@@ -0,0 +1,13 @@
+//
+//  CouponStatusManager.h
+//  GitPractice
+//
+//  Created by 能登 要 on 2014/08/29.
+//  Copyright (c) 2014年 Classmethod, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CouponStatusManager : NSObject
+
+@end
---
+++ CouponStatusManager.m
@@ -0,0 +1,13 @@
+//
+//  CouponStatusManager.m
+//  GitPractice
+//
+//  Created by 能登 要 on 2014/08/29.
+//  Copyright (c) 2014年 Classmethod, Inc. All rights reserved.
+//
+
+#import "CouponStatusManager.h"
+
+@implementation CouponStatusManager
+
+@end
