43b06b9e734c26b29b564c4d846143df321bb933
notoroid <noto@irimasu.com>
2014-08-29 13:52:41 +0900

    MyView を追加

---
+++ GitPractice/MyView.m
@@ -0,0 +1,21 @@
+//
+//  MyView.m
+//  GitPractice
+//
+//  Created by 能登 要 on 2014/08/29.
+//  Copyright (c) 2014年 Classmethod, Inc. All rights reserved.
+//
+
+#import "MyView.h"
+
+@implementation MyView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
