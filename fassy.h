c5c01a8020922aa092f9b893dee2cb34c3b4b1c6
cm-ttamiya <tamiya.tokihiko@classmethod.jp>
2014-08-29 14:57:50 +0900

    fassyを追加

---
+++ fassy.h
@@ -0,0 +1,13 @@
+//
+//  fassy.h
+//  GitPractice
+//
+//  Created by tamiya.tokihiko on 2014/08/29.
+//  Copyright (c) 2014年 Classmethod, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface fassy : NSView
+
+@end
