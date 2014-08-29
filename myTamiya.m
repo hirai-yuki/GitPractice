d874bac1714fdd2632b50070d8e5abbf3313e299
cm-ttamiya <tamiya.tokihiko@classmethod.jp>
2014-08-29 13:53:18 +0900

    myTamiya.m myTamiya.h

---
+++ myTamiya.m
@@ -0,0 +1,19 @@
+//
+//  myTamiya.m
+//  GitPractice
+//
+//  Created by tamiya.tokihiko on 2014/08/29.
+//  Copyright (c) 2014年 Classmethod, Inc. All rights reserved.
+//
+
+#import "myTamiya.h"
+
+@implementation myTamiya
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end
